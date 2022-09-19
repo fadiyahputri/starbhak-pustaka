00335b7cd5f3f45bcb60330959e5ffddf5139937
fadiyahputri <fadiyah089putri@gmail.com>
2022-09-19 09:18:46 +0700

    first commit

--- masterdata/dashboard.php
+++ masterdata/dashboard.php
@@ -9,6 +9,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+
+    <!-- link css -->
+    <link rel="stylesheet" href="../css/styledashboard.css">
 </head>
 <body>
     
--- masterdata/datamurid.php
+++ masterdata/datamurid.php
@@ -9,6 +9,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Murid</title>
+
+    <!-- link css -->
+    <link rel="stylesheet" href="../css/styledatamurid.css">
 </head>
 <body>
     
